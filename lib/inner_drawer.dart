6ca921d6ffa9feb131ebf9c680e0b88a867e7a68
Korbinian Zehentner <korbinian.zehentner@codingpassion.net>
2023-07-17 15:28:24 +0200

    replaced deprecated backgroundColor with colorScheme.background

--- lib/inner_drawer.dart
+++ lib/inner_drawer.dart
@@ -582,7 +582,7 @@ class InnerDrawerState extends State<InnerDrawer>
     return Container(
       decoration: widget.backgroundDecoration ??
           BoxDecoration(
-            color: Theme.of(context).backgroundColor,
+            color: Theme.of(context).colorScheme.background,
           ),
       child: Stack(
         alignment: _drawerInnerAlignment!,
